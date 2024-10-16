08affa2bfabf6ac371dcaab6b731ecfee66466fd
gimmin-u <kimlg2015@naver.com>
2024-10-17 03:29:39 +0900

    목록 화면 작성

--- lib/ㅣist_screen.dart
+++ lib/ㅣist_screen.dart
@@ -1,8 +1,30 @@
-class  extends StatelessWidget {
-  const ({super.key});
+import 'package:flutter/material.dart';
+
+class ListScreen extends StatelessWidget {
+  const ListScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Todo list 리스트'),
+      ),
+      body: ListView(
+        children: const [
+          ListTile(
+            title: Text('title 1'),
+            subtitle: Text('subtitle 1'),
+          ),
+          ListTile(
+            title: Text('title 2'),
+            subtitle: Text('subtitle 2'),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {  },
+        child: const Icon(Icons.add),
+      ),
+    );
   }
 }
