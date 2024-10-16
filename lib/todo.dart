0c6fa8b1dbd15cd7c7e7d52f4db9f31707180427
gimmin-u <kimlg2015@naver.com>
2024-10-17 05:47:03 +0900

    Hive 사용

--- lib/todo.dart
+++ lib/todo.dart
@@ -1,5 +1,16 @@
-class Todo {
+import 'package:hive_flutter/adapters.dart';
+
+part 'todo.g.dart';
+
+@HiveType(typeId: 0)
+class Todo extends HiveObject{
+  @HiveField(0)
+  int? id;
+
+  @HiveField(1)
   String title;
+
+  @HiveField(2)
   int dateTime;
 
   Todo({
