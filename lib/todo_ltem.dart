8b123a90e6c2079c4462edd9f3069e2d89c514c3
gimmin-u <kimlg2015@naver.com>
2024-10-18 05:06:52 +0900

    할일 리스트 수정 기능 추가

--- lib/todo_ltem.dart
+++ lib/todo_ltem.dart
@@ -12,36 +12,41 @@ class TodoLtem extends StatelessWidget {
     required this.todo,
     required this.onTap,
     required this.onDelete,
-    }) :
-    super(key: key);
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      onTap:() {
-        onTap(todo);
+      onTap: () {
+        onTap(todo); // 클릭 시 체크 상태 변경
       },
-      leading: todo.isDone
-        ? const Icon(Icons.check_circle, color: Colors.blueAccent)
-        : const Icon(Icons.check_circle_outline),
+      leading: Checkbox(
+        value: todo.isDone,
+        onChanged: (bool? value) {
+          onTap(todo); // 체크박스 클릭 시 상태 변경 처리
+        },
+      ), // 체크박스를 맨 앞에 위치시킴
       title: Text(
-          todo.title,
-          style: TextStyle(color:  todo.isDone ? Colors.grey : Colors.black),
+        todo.title,
+        style: TextStyle(
+          color: todo.isDone ? Colors.grey : Colors.black,
+          decoration: todo.isDone ? TextDecoration.lineThrough : null,
+        ),
       ),
       subtitle: Text(
-          DateFormat.yMMMEd().
-          format(DateTime.
-          fromMillisecondsSinceEpoch(todo.dateTime)),
-        style: TextStyle(color:  todo.isDone ? Colors.grey : Colors.black),
+        DateFormat.yMMMEd().format(
+          DateTime.fromMillisecondsSinceEpoch(todo.dateTime),
+        ),
+        style: TextStyle(color: todo.isDone ? Colors.grey : Colors.black),
       ),
-        trailing:todo.isDone ?
-        GestureDetector(
-          onTap: () {
-            onDelete(todo);
-          },
-          child: const Icon(Icons.delete_forever),
-        )
-        : null,
+      trailing: todo.isDone
+          ? GestureDetector(
+        onTap: () {
+          onDelete(todo);
+        },
+        child: const Icon(Icons.delete_forever),
+      )
+          : null,
     );
   }
 }
